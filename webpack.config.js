5f89b1e1efa6a32a24176ba098c83db1c7e8b5e0
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-17 15:07:47 -0400

    added port for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  resolve: {
+    fallback: {
+      "http": require.resolve("stream-http")
+    }
+}
+  
 };
